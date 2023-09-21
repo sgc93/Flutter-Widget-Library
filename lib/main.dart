b32aecc621017e3f03d9c3d2444827a97c452f54
sgc93 <smachewgedefaw@gmail.com>
2023-09-21 06:06:53 -0900

    call tween animated widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_widgets/widgets/animated.dart';
+import 'package:learn_widgets/widgets/tween_animation.dart';
 
 void main(){
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const AnimatedWidgets(),
+      home: const TweenAnimation(),
     );
   }
 }
