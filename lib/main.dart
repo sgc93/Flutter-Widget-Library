52f30988bf3080c19a95c28c013fdd23d97a0ff4
sgc93 <smachewgedefaw@gmail.com>
2023-09-22 13:04:50 -0900

    call boxShadow widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:learn_widgets/widgets/animation_controller.dart';
-import 'package:learn_widgets/widgets/tween_animation.dart';
+import 'package:learn_widgets/widgets/box_shadow.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
@@ -10,13 +9,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'learn flutter widgets',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: const AnimeController(),
+      home: const Neumorphism(),
     );
   }
 }
