e1e10e7f6d499388219668119eeab6fbc0a54c14
sgc93 <smachewgedefaw@gmail.com>
2023-09-23 08:20:24 -0900

    neumorphism effect with boxshadow

--- lib/widgets/box_shadow.dart
+++ lib/widgets/box_shadow.dart
@@ -15,28 +15,81 @@ class Neumorphism extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Container(
-          height: 159,
-          width: 159,
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade600,
-                offset: const Offset(5, 5),
-                blurRadius: 8,
-              ),
-              const BoxShadow(
-                color: Colors.white,
-                offset: Offset(-5, -5),
-                blurRadius: 9,
-              ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _getChildren('1'),
+              _getChildren('2'),
+              _getChildren('3'),
+              _getChildren('+'),
             ],
           ),
-          child: const Icon(
-            Icons.apple,
-            size: 70,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _getChildren('4'),
+              _getChildren('5'),
+              _getChildren('6'),
+              _getChildren('-'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _getChildren('7'),
+              _getChildren('8'),
+              _getChildren('9'),
+              _getChildren('*'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _getChildren('.'),
+              _getChildren('0'),
+              _getChildren('='),
+              _getChildren('/'),
+            ],
+          ),
+        ],
+      )),
+    );
+  }
+
+  _getBoxDecoration() {
+    return BoxDecoration(
+      color: Colors.grey[300],
+      borderRadius: BorderRadius.circular(20),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.shade600,
+          offset: const Offset(5, 5),
+          blurRadius: 8,
+        ),
+        const BoxShadow(
+          color: Colors.white,
+          offset: Offset(-5, -5),
+          blurRadius: 9,
+        ),
+      ],
+    );
+  }
+
+  Widget _getChildren(String icon) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: _getBoxDecoration(),
+      child: Center(
+        child: Text(
+          icon,
+          style: const TextStyle(
+            fontSize: 30,
           ),
         ),
       ),
