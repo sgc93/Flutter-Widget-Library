e01618b5b8d15e7a5e2b245cdab9c0bdf878168a
sgc93 <smachewgedefaw@gmail.com>
2023-09-29 11:46:11 -0900

    create customize endDrower for settengs menu

--- lib/widgets/drawer/end_drawer.dart
+++ lib/widgets/drawer/end_drawer.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MyEndDrawr extends StatelessWidget {
-  const MyEndDrawr({super.key});
+class MyEndDrawer extends StatelessWidget {
+  const MyEndDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Drawer(
+      backgroundColor: const Color.fromRGBO(53, 58, 63, 1),
+      child: ListView(
+        children: const [
+          ListTile(
+            title: Text(
+              'Settings',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('Upload status'),
+            subtitle: Text('uploading your status to codergames.com'),
+          ),
+          ListTile(
+            title: Text('Show instant score'),
+            subtitle: Text('instant score will be displayed'),
+            trailing: Icon(
+              Icons.toggle_off,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text('Developer Mode'),
+            subtitle: Text('developer mode is on'),
+            trailing: Icon(
+              Icons.toggle_on,
+              color: Colors.white,
+            ),
+          ),
+          ListTile(
+            title: Text('About'),
+          ),
+          SizedBox(
+            height: 440,
+          ),
+          Center(child: Text('v1.0.0')),
+        ],
+      ),
+    );
   }
 }
