8c4f47a903ad74c9723ec4e7fe4831009e6d9348
sgc93 <smachewgedefaw@gmail.com>
2023-09-29 10:27:51 -0900

    call a Scaffold with Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_widgets/widgets/box_shadow.dart';
+import 'package:learn_widgets/widgets/drawer/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: Neumorphism(),
+      home: DrawerHome(),
     );
   }
 }
