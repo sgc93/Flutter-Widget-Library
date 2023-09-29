dcb5fde6331d4ba20b80ba6dff41231419975a6d
sgc93 <smachewgedefaw@gmail.com>
2023-09-29 09:32:38 -0900

    as a homepage

---
+++ lib/widgets/drawer/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrawerHome extends StatelessWidget {
+  const DrawerHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
