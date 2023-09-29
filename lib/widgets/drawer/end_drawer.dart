c44146a9260f80cb685ca98608f1fd3a667a7550
sgc93 <smachewgedefaw@gmail.com>
2023-09-29 09:33:10 -0900

    create endDrawer widget

---
+++ lib/widgets/drawer/end_drawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyEndDrawr extends StatelessWidget {
+  const MyEndDrawr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
