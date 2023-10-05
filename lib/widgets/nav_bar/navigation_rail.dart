2901a869369395c839ed0400327a8697ace9b228
sgc93 <smachewgedefaw@gmail.com>
2023-10-05 07:28:05 -0900

    create initial file for NavigationRail Widget

---
+++ lib/widgets/nav_bar/navigation_rail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavRail extends StatefulWidget {
+  const NavRail({super.key});
+
+  @override
+  State<NavRail> createState() => _NavRailState();
+}
+
+class _NavRailState extends State<NavRail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
