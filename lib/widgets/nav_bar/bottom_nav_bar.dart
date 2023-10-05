ad470832f24fe09d0981d2a35a817cd8ac9bb883
sgc93 <smachewgedefaw@gmail.com>
2023-10-05 10:55:06 -0900

    create file for BottomNavigationBar illustration

---
+++ lib/widgets/nav_bar/bottom_nav_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('BottomNavigationBar'),
+      ),
+    );
+  }
+}
