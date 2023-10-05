bb3aa6ca5f4ba4d4dc03fd36d563e10028a26fe9
sgc93 <smachewgedefaw@gmail.com>
2023-10-05 10:25:26 -0900

    define NavigationRail Widget

--- lib/widgets/nav_bar/navigation_rail.dart
+++ lib/widgets/nav_bar/navigation_rail.dart
@@ -8,6 +8,7 @@ class NavRail extends StatefulWidget {
 }
 
 class _NavRailState extends State<NavRail> {
+  final int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +18,42 @@ class _NavRailState extends State<NavRail> {
         title: const Text('#NavigationRail'),
         elevation: 0,
       ),
+      bottomNavigationBar: NavigationRail(
+          destinations: _navigationRailDestionations(),
+          selectedIndex: _selectedIndex),
     );
+  }
+
+  List<NavigationRailDestination> _navigationRailDestionations() {
+    return [
+      const NavigationRailDestination(
+        icon: Icon(
+          Icons.home,
+          color: Colors.white,
+        ),
+        label: Text('HOME'),
+      ),
+      const NavigationRailDestination(
+        icon: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        label: Text('ADD'),
+      ),
+      const NavigationRailDestination(
+        icon: Icon(
+          Icons.settings,
+          color: Colors.white,
+        ),
+        label: Text('SETTINGS'),
+      ),
+      const NavigationRailDestination(
+        icon: Icon(
+          Icons.person,
+          color: Colors.white,
+        ),
+        label: Text('ACCOUNT'),
+      ),
+    ];
   }
 }
