5a7ebdf8da9b5e51c46de35e9c1a5785b157c471
sgc93 <smachewgedefaw@gmail.com>
2023-10-05 11:04:14 -0900

    handle onTap callBack funciton

--- lib/widgets/nav_bar/bottom_nav_bar.dart
+++ lib/widgets/nav_bar/bottom_nav_bar.dart
@@ -8,14 +8,24 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[300],
       appBar: AppBar(
         backgroundColor: Colors.amber,
         title: const Text('BottomNavigationBar'),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.deepOrange,
+        backgroundColor: Colors.amber,
+        currentIndex: _currentIndex,
         items: _navDestination(),
       ),
     );
